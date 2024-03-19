4deb492094752e3ba44cc88f6f82f3bf5de8983a
H4ntz-M4d <muhadiahmad71@gmail.com>
2024-03-19 15:22:04 +0700

    third commit

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -14,13 +14,15 @@ class QuestionsSummary extends StatelessWidget{
           return Row(
             children: [
               Text(((data['question_index'] as int) + 1).toString()),
-              Column(children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5,),
+              Expanded(
+                child: Column(children: [
+                  Text(data['question'] as String, style: const TextStyle(color: Colors.white),),
+                  const SizedBox(height: 5,),
                 
-                Text(data['user_answer'] as String),
-                Text(data['correct_answer'] as String)
-              ],)
+                  Text(data['user_answer'] as String, style: const TextStyle(color: Colors.white),),
+                  Text(data['correct_answer'] as String, style: const TextStyle(color: Colors.white),)
+                ],),
+              )
             ],);
         }).toList(),  
     );
