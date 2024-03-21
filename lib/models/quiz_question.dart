0c013d34424eff114765485a2ab4315007342946
H4ntz-M4d <muhadiahmad71@gmail.com>
2024-03-21 12:13:38 +0700

    additional code

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,6 +4,9 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
+  //You can use a method function in bellow or you can use approach getter for the code
+  //List<String> getShuffleAnswers(){}
+
   List<String> get shuffleAnswers{
     final shuffledList = List.of(answers);
     shuffledList.shuffle();
